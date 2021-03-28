182293f0c030ec2265a078da763c9aa0958e975b
Youra Halloween <bestletter@inbox.ru>
2021-03-29 00:04:39 +0500

    add size components

--- components/collapse/component.d.ts
+++ components/collapse/component.d.ts
@@ -1,5 +1,7 @@
-import { MDCList } from "@material/list/component";
+import { MDCList } from '@material/list/component';
 declare class TCollapse extends MDCList {
     constructor(root: Element, ...args: any[]);
+    private setClassByTimeout;
+    isOpen(element: Element): boolean;
 }
 export { TCollapse };
